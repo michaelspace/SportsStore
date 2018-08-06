84ff8632e075ce6ff9085521c8807d73c3a3e23a
michaelspace <kmichal1797@gmail.com>
2018-08-06 17:37:46 +0200

    Added repository.

---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository: IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product {Name = "Piłka nożna", Price = 25M},
+            new Product {Name = "Deska surfingowa", Price = 180M},
+            new Product {Name = "Buty do biegania", Price = 90M}
+        }.AsQueryable<Product>();
+    }
+}
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -6,19 +6,19 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using SportsStore.Models;
 
 namespace SportsStore
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IProductRepository, FakeProductRepository>();
+
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
