7bc421ccc3ecd2b8f5e9e3a3bdfbcad0b44b225e
michaelspace <kmichal1797@gmail.com>
2018-08-06 17:44:40 +0200

    Added controller.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public ViewResult List() => View(_repository.Products);
+    }
+}
