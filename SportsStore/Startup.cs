ee6be317687b466b0c3b78361a49cb3080afec62
michaelspace <kmichal1797@gmail.com>
2018-08-06 17:25:54 +0200

    Initial commit.

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -15,6 +15,7 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +26,10 @@ namespace SportsStore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            app.UseStatusCodePages();
+            app.UseStaticFiles(); // włącza obsługę treści statycznej znajdującej się w katalogu wwwroot
+            app.UseMvc(routes => {
+
             });
         }
     }
