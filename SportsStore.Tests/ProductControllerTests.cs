683cc4287a7f7dbb4412389a289f1261b6d1ad58
michaelspace <kmichal1797@gmail.com>
2018-08-09 14:58:55 +0200

    Added pagination and tests.

---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Paginate()
+        {
+            // arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            }).AsQueryable<Product>());
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // act
+            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+
+            // assert
+            Product[] prodArray = result.ToArray();
+
+            Assert.True(prodArray.Length == 2);
+
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
+    }
+}
--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -10,12 +10,18 @@ namespace SportsStore.Controllers
     public class ProductController : Controller
     {
         private readonly IProductRepository _repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repo)
         {
             _repository = repo;
         }
 
-        public ViewResult List() => View(_repository.Products);
+        public ViewResult List(int productPage = 1) => 
+            View(_repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize)
+                );
     }
 }
